7835410f61ccc899ea8b443aefc1a6233e2c1122
Lazar Ristic <lazarristic95@gmail.com>
2021-08-14 10:00:14 +0200

    Added Utils Singleton and moved some logic

---
+++ src/Utils.java
@@ -0,0 +1,28 @@
+public class Utils {
+
+    public static final String EMAIL_PATTERN =
+            "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
+                    + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+
+    private static Utils instance;
+
+    private Utils() {
+    }
+
+    public static Utils getInstance() {
+        if (instance == null) {
+            synchronized (Utils.class) {
+                if (instance == null) {
+                    instance = new Utils();
+                }
+            }
+        }
+        return instance;
+    }
+
+    public String formatNameAndEmail(String name, String email) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name).append(" <").append(email).append(">");
+        return stringBuilder.toString();
+    }
+}
