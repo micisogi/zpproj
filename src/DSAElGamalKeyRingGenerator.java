b10e199c364f52bacf3be4189f78f0acb6f31201
Lazar Ristic <lazarristic95@gmail.com>
2021-08-19 17:49:06 +0200

    Changes made for read secret

--- src/DSAElGamalKeyRingGenerator.java
+++ src/DSAElGamalKeyRingGenerator.java
@@ -1,7 +1,5 @@
-import org.bouncycastle.bcpg.ArmoredOutputStream;
+import models.User;
 import org.bouncycastle.bcpg.HashAlgorithmTags;
-import org.bouncycastle.bcpg.UserAttributePacket;
-import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.openpgp.*;
 import org.bouncycastle.openpgp.operator.PGPDigestCalculator;
 import org.bouncycastle.openpgp.operator.jcajce.JcaPGPContentSignerBuilder;
@@ -10,10 +8,8 @@ import org.bouncycastle.openpgp.operator.jcajce.JcaPGPKeyPair;
 import org.bouncycastle.openpgp.operator.jcajce.JcePBESecretKeyEncryptorBuilder;
 import utils.KeyRingHelper;
 import utils.Utils;
-import models.*;
+
 import javax.crypto.spec.DHParameterSpec;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.math.BigInteger;
 import java.security.*;
@@ -21,7 +17,9 @@ import java.util.Date;
 
 public class DSAElGamalKeyRingGenerator {
 
-    public void DSAElGamalKeyRingGenerator(){}
+    public void DSAElGamalKeyRingGenerator() {
+    }
+
     public void generateDSAELGamalKeyRing(Integer dsaKeySize, Integer elGamalKeySize, String name,
                                           String email, String passPhrase)
             throws NoSuchProviderException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, IOException, PGPException {
@@ -33,14 +31,11 @@ public class DSAElGamalKeyRingGenerator {
         BigInteger g = new BigInteger("153d5d6172adb43045b68ae8e1de1070b6137005686d29d3d73a7749199681ee5b212c9b96bfdcfa5b20cd5e3fd2044895d609cf9b410b7a0f12ca1cb9a428cc", 16);
         BigInteger p = new BigInteger("9494fec095f3b85ee286542b3836fc81a5dd0a0349b4c239dd38744d488cf8e31db8bcb7d33b41abb9e5a33cca9144b1cef332c94bf0573bf047a3aca98cdf3b", 16);
 
-        DHParameterSpec elParams = new DHParameterSpec(p, g);
+        DHParameterSpec elParams = new DHParameterSpec(p, g, elGamalKeySize);
 
         elgKpg.initialize(elParams);
 
         KeyPair elgKp = elgKpg.generateKeyPair();
-        FileOutputStream outPublic = new FileOutputStream("D:\\exported_pub.asc");
-        ArmoredOutputStream outSecret = new ArmoredOutputStream(new FileOutputStream("D:\\exported_secret.asc"));
-
         PGPKeyPair dsaKeyPair = new JcaPGPKeyPair(PGPPublicKey.DSA, dsaKp, new Date());
         PGPKeyPair elgKeyPair = new JcaPGPKeyPair(PGPPublicKey.ELGAMAL_ENCRYPT, elgKp, new Date());
         PGPDigestCalculator sha1Calc = new JcaPGPDigestCalculatorProviderBuilder().build().get(HashAlgorithmTags.SHA1);
@@ -60,19 +55,4 @@ public class DSAElGamalKeyRingGenerator {
 
 
     }
-
-
-//   public  void generateDsaKeyPairWithParameters(Integer dsaKeySize, Integer elGamalKeySize,String name,String email) throws Exception, IOException, InvalidKeyException, NoSuchProviderException, SignatureException, PGPException {
-//        Security.addProvider(new BouncyCastleProvider());
-//        KeyPairGenerator dsaKpg = KeyPairGenerator.getInstance("DSA", "BC");
-//        dsaKpg.initialize(1024);
-//
-//        KeyPair dsaKp = dsaKpg.generateKeyPair();
-//
-//        FileOutputStream secretOutputStream = new FileOutputStream("secret.asc");
-//        FileOutputStream out2 = new FileOutputStream("pub.asc");
-//        char[] zakucano = {'s', 'u', 's'};
-//        KeyRingGenerator.exportKeyPair(secretOutputStream, out2, dsaKp, "LAZAR <hans.mueller@mail.com>", zakucano, "lazar@mojmail.com", 0);
-//    }
-
 }
